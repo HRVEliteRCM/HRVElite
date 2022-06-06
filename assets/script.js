a8a53a3ccee9627569377af177c41ecfa11e4162
HRVEliteRCM <106903582+HRVEliteRCM@users.noreply.github.com>
2022-06-07 01:13:16 +0530

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,13 @@
-// navbar togglemenu start
-var navLinks = document.getElementById("navLinks");
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
 
-function showMenu() {
-    navLinks.style.right = "0";
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
 }
 
-function hideMenu() {
-    navLinks.style.right = "-200px";
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
 }
 // navbar togglemenu end
